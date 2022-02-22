1d09279a7e05cf7779435d11ccfed5fcff171e71
PWojszko <piotr.wojszko@gmail.com>
2022-02-22 20:52:23 +0100

    Updates

---
+++ precache-manifest.9827e9c78ef135c432ff0a513bdd5b6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6982cdd993b69570cfd",
+    "url": "/react-router-deployment/static/css/main.e8632aae.chunk.css"
+  },
+  {
+    "revision": "d6982cdd993b69570cfd",
+    "url": "/react-router-deployment/static/js/main.d6982cdd.chunk.js"
+  },
+  {
+    "revision": "47f6e527eb21a4046a1d",
+    "url": "/react-router-deployment/static/js/runtime~main.47f6e527.js"
+  },
+  {
+    "revision": "328dd81371343b01b831",
+    "url": "/react-router-deployment/static/js/2.328dd813.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-router-deployment/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-router-deployment/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-router-deployment/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "d128ac28e2f232bde6143ebf77a704b8",
+    "url": "/react-router-deployment/index.html"
+  }
+];
